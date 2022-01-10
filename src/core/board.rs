7fa074c97d050728e45d6bc58f5cebd49447f997
Romain TRUCHI <romain.truchi.06@gmail.com>
2022-01-10 12:01:47 +0100

    Board is too big!!!

--- src/core/board.rs
+++ src/core/board.rs
@@ -5,6 +5,24 @@ use std::ops::IndexMut;
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub struct Board(pub [[Option<Piece>; SIZE]; SIZE]);
 
+impl Default for Board {
+    fn default() -> Self {
+        macro_rules! file {
+            ($piece:expr) => {
+                [Some((Red, $piece)), None, None, None, Some((Blue, $piece))]
+            };
+        }
+
+        Self([
+            file!(Pawn(0)),
+            file!(Pawn(1)),
+            file!(King),
+            file!(Pawn(2)),
+            file!(Pawn(3)),
+        ])
+    }
+}
+
 impl Index<Square> for Board {
     type Output = Option<Piece>;
 
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,41 @@ mod core;
 pub use self::core::*;
 
 fn main() {
-    let board = Board([
-        [Some((Red, King)), None, None, None, None],
-        [None, None, None, None, None],
-        [None, None, None, None, None],
-        [None, None, None, None, None],
-        [None, None, None, None, None],
-    ]);
-
-    // dbg!(board[Square(A, One)]);
-    // dbg!(CARDS);
-    dbg!(std::mem::size_of::<Play>());
-
     debug_cards();
+    dbg!(std::mem::size_of::<Play>());
+    dbg!(std::mem::size_of::<Move>());
+    dbg!(std::mem::size_of::<Card>());
+    dbg!(std::mem::size_of::<Square>());
+    dbg!(std::mem::size_of::<Option<Square>>());
+    dbg!(std::mem::size_of::<Side>());
+    dbg!(std::mem::size_of::<Piece>());
+    dbg!(std::mem::size_of::<Option<Piece>>());
+    dbg!(std::mem::size_of::<Option<Piece>>() * SIZE * SIZE);
+    dbg!(std::mem::size_of::<[Option<Piece>; SIZE * SIZE]>());
+    dbg!(std::mem::size_of::<Board>());
+    dbg!(std::mem::size_of::<Game>()); // 856...
+    dbg!(std::mem::size_of::<P>());
+    dbg!(std::mem::size_of::<Option<P>>());
+    dbg!(std::mem::size_of::<Option<P>>() * SIZE * SIZE);
+    dbg!(std::mem::size_of::<[Option<P>; SIZE * SIZE]>());
+
+    enum P {
+        King,
+        PawnA,
+        PawnB,
+        PawnD,
+        PawnE,
+    }
+
+    let board = Board::default();
+    // dbg!(board[Square(A, One)]);
+    // dbg!(board[Square(B, One)]);
+    // dbg!(board[Square(C, One)]);
+    // dbg!(board[Square(D, One)]);
+    // dbg!(board[Square(E, One)]);
+    // dbg!(board[Square(A, Five)]);
+    // dbg!(board[Square(B, Five)]);
+    // dbg!(board[Square(C, Five)]);
+    // dbg!(board[Square(D, Five)]);
+    // dbg!(board[Square(E, Five)]);
 }
